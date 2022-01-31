4f41c70c196410a89a878e35a4857843466a5b52
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2022-01-31 14:09:57 -0500

    Add test.

---
+++ test/test.js
@@ -0,0 +1,10 @@
+/*!
+ * Copyright (c) 2022 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+const bedrock = require('bedrock');
+require('bedrock-vc-status-list-context');
+
+require('bedrock-test');
+bedrock.start();
